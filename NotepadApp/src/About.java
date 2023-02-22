b57ff7c3d637b9dbe73097eccbdbeca4367695fe
shanalawaze11 <99750524+shanalawaze11@users.noreply.github.com>
2023-02-22 12:27:29 +0530

    First Commit

---
+++ NotepadApp/src/About.java
@@ -0,0 +1,33 @@
+
+import javax.swing.*;
+import java.awt.Font;
+public class About extends JFrame {
+
+
+		
+		About()
+		{
+			setBounds(100,100,500,400);
+			setTitle("About Notepad Application");
+			setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+			ImageIcon icon = new ImageIcon(getClass().getResource("Notepad_Icon_72.png"));
+			setIconImage(icon.getImage());
+			
+			setLayout(null);
+			
+			JLabel iconLabel=new JLabel(new ImageIcon(getClass().getResource("Notepad_Icon_72.png")));
+			iconLabel.setBounds(100,50,80,80);
+			add(iconLabel);
+			
+			JLabel textLabel=new JLabel("<html>Welcome to Notepad<br>Notepad is a simple text editor for Microsoft Windows and a basic text-editing program which enables computer users to create documents<br>All rights reserved@2021</html>");
+			textLabel.setBounds(100,50,350,300);
+			textLabel.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,16));
+			
+			add(textLabel);
+		}
+		public static void main(String[] args) {
+		       new About().setVisible(true);
+		    }
+	
+
+}
